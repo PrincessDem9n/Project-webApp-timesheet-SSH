018600d51061e3d51476cd9c63da40edfa49156a
PrincessDem9n <106153088+PrincessDem9n@users.noreply.github.com>
2025-02-21 22:37:25 +0800

    Add files via upload
    add missing test file

---
+++ src/test/java/com/dvops/maven/eclipse/NewTeset.java
@@ -0,0 +1,54 @@
+package com.dvops.maven.eclipse;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+//import necessary Selenium WebDriver classes
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class NewTeset{
+  //declare Selenium WebDriver
+	private WebDriver webDriver;
+  
+	@BeforeTest
+	public void beforeTest() {
+		  //Setting system properties of ChromeDriver
+		  //to amend directory path base on your local file path
+			String chromeDriverDir = "C:\\Program Files\\Google\\Chrome\\chromedriver.exe";
+			System.setProperty("webDriver.chrome.driver", chromeDriverDir);
+		  //initialize FirefoxDriver at the start of test
+			webDriver = new ChromeDriver();
+			}
+	
+   @Test  
+   public void checkTitle() {
+	  //Load website as a new page
+	   webDriver = new ChromeDriver();
+	   webDriver.navigate().to("http://localhost:8080/Project/userservlets/dashboard");
+	  
+	  //Assert the title to check that we are indeed in the correct website
+	   Assert.assertEquals(webDriver.getTitle(), "Mott MacDonald Timesheet App");
+	   System.out.println("title: "+webDriver.getTitle());
+   		}
+	
+   @Test
+   public void testButtonPresence() {
+	   webDriver = new ChromeDriver();
+	   webDriver.navigate().to("http://localhost:8080/Project/userservlets/dashboard");
+        // Find the button by its name
+	   WebElement button = webDriver.findElement(By.linkText("Add New Employee"));
+        button.click();
+        // Verify the title of the new page
+        Assert.assertEquals(webDriver.getTitle(), "Mott MacDonald Timesheet App");
+        System.out.println("title: "+webDriver.getTitle());
+        }
+  
+  @AfterTest
+  public void afterTest() {
+	  //Quit the ChromeDriver and close all associated window at the end of test
+	  webDriver.close();			
+  }
+ }
