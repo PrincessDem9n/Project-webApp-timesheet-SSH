ec3d15deaed04837fa56613fb567c5763c031bad
PrincessDem9n <skeetplaying@hotmail.com>
2025-02-26 14:47:51 +0800

    change to pipeline.java

--- src/main/Java/com/dvops/maven/eclipse/Pipeline.java
+++ src/main/Java/com/dvops/maven/eclipse/Pipeline.java
@@ -3,7 +3,7 @@ package com.dvops.maven.eclipse;
 public class Pipeline {
 //declare attributes
 	protected String empID;
-	protected String staffID;
+	protected String staffname = "Neil";
 
 	//testing if the pipeline will update with the changes
 	public String getEmpID() {
