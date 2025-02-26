83a79fac7b66d45bb3572154da2cc36f2975421f
PrincessDem9n <skeetplaying@hotmail.com>
2025-02-26 14:50:58 +0800

    adding new string variable

--- src/main/Java/com/dvops/maven/eclipse/Pipeline.java
+++ src/main/Java/com/dvops/maven/eclipse/Pipeline.java
@@ -4,6 +4,7 @@ public class Pipeline {
 //declare attributes
 	protected String empID;
 	protected String staffname = "Neil";
+	protected String grade;
 
 	//testing if the pipeline will update with the changes
 	public String getEmpID() {
